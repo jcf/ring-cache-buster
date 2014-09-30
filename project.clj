75496916af3137642cb5f1be10fbcba3d99f0037
James Conroy-Finn <james@logi.cl>
2014-09-30 11:40:58 +0100

    Prepare for deploy to Clojars

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [ring/ring-core "1.3.0-RC1"]]
+  :deploy-repositories [["releases" :clojars]]
   :plugins [[codox "0.8.5"]]
   :codox {:project {:name "Ring-Cache-Buster"}}
   :profiles {:dev {:dependencies [[ring-mock "0.1.5"]]}})
