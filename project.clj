b88d2795b28b354b16dba892c958328cda066e32
James Conroy-Finn <james@logi.cl>
2014-09-30 11:45:03 +0100

    Version 0.3.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject listora/ring-cache-buster "0.2.0"
+(defproject listora/ring-cache-buster "0.3.0-SNAPSHOT"
   :description "Ring middleware that tells the world to cache nothing"
   :url "https://github.com/listora/ring-cache-buster"
   :license {:name "The MIT License"
